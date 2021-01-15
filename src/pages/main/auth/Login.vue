933dac71896f1406e005bd85acff9c64d7b15b87
mashrabjon <akbarov.dev@gmail.com>
2021-01-15 18:39:31 +0500

    login page added

---
+++ src/pages/main/auth/Login.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="row justify-center">
+    <div class="column self-center">
+      <q-card style="width: 360px" flat>
+        <q-card-section class="row justify-center">
+          <!-- <span>{{ $t("auth._self") }}</span> -->
+        </q-card-section>
+        <q-form>
+          <q-card-section>
+            <!-- LOGIN -->
+            <q-input
+              dense
+              square
+              outlined
+              clearable
+              v-model.trim="credentials.username"
+              :placeholder="$t('auth.username')"
+            >
+              <template v-slot:prepend>
+                <q-icon name="account_circle" />
+              </template>
+            </q-input>
+            <!-- Password -->
+            <q-input
+              dense
+              square
+              outlined
+              clearable
+              v-model="credentials.password"
+              :placeholder="$t('auth.password')"
+              v-on:keyup.enter="handleSubmit()"
+              :type="showPass ? 'text' : 'password'"
+            >
+              <template v-slot:prepend>
+                <q-icon name="vpn_key" />
+              </template>
+              <template v-slot:append>
+                <q-btn
+                  round
+                  dense
+                  flat
+                  :icon="showPass ? 'o_visibility' : 'o_visibility_off'"
+                  @click="showPass = !showPass"
+                />
+              </template>
+            </q-input>
+            <q-select
+              outlined
+              class="col-xs-12 col-sm-6 col-md-6"
+              v-model="credentials.lang"
+              :options="langsList"
+              option-value="value"
+              option-label="text"
+              map-options
+            >
+              <template v-slot:prepend>
+                <q-icon name="language" />
+              </template>
+            </q-select>
+          </q-card-section>
+          <q-card-section>
+            <q-btn
+              class="full-width"
+              color="primary"
+              @click="handleSubmit()"
+              v-on:keyup.enter="handleSubmit()"
+              >{{ $t("auth.signin") }}</q-btn
+            >
+          </q-card-section>
+        </q-form>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showPass: false,
+      credentials: {
+        username: null,
+        password: null,
+        lang: { text: "Русский", value: "ru" }
+      },
+      message: "",
+      langsList: [
+        { text: "Русский", value: "ru" }, // uz, ru, en
+        { text: "Узбек крилл", value: "uzkr" },
+        { text: "English", value: "en" },
+        { text: "O'zbek Lotin", value: "uz" }
+      ],
+      loginError: false
+    };
+  },
+  methods: {
+    handleSubmit() {
+      //Perform a simple validation that email and password have been typed in
+      if (!!this.credentials.username && !!this.credentials.password) {
+        //LoadingService.showLoadingHourGlass();
+
+        AuthService.login(this.credentials, res => {
+          if (res) {
+            // LoadingService.hideLoading();
+            this.clearForm();
+            // NotifyService.showSuccessMessage("Successfully logged in");
+          } else {
+            //LoadingService.hideLoading();
+            //NotifyService.showErrorMessage("Error in login");
+          }
+        });
+      } else {
+        //LoadingService.hideLoading();
+        //NotifyService.showErrorMessage("Enter credentials correctly");
+      }
+    },
+    clearForm() {
+      this.credentials.username = "";
+      this.credentials.password = "";
+      //this.$v.credentials.$reset(); // TODO resetting validation
+    },
+    onLangChange(lang) {
+      //  this.$i18n.locale = lang;
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,26 @@
+const Page404 = () => import("pages/extras/Error404.vue");
+const LoginPage = () => import("pages/main/auth/Login");
+
 const routes = [
   {
     path: "/",
     component: () => import("layouts/Main.vue"),
     children: [{ path: "", component: () => import("pages/Index.vue") }]
   },
-
+  {
+    path: "/login",
+    name: "Login Page",
+    component: LoginPage,
+    meta: {
+      public: true, // Allow access to even if not logged in
+      onlyWhenLoggedOut: true
+    }
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
     path: "*",
-    component: () => import("pages/Error404.vue")
+    component: Page404
   }
 ];
 
