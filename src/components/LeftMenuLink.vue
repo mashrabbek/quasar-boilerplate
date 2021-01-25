26c6a8c80c57090be8160d1bbc23921103656217
mashrabjon <akbarov.dev@gmail.com>
2021-01-25 10:43:33 +0500

    left menu fixed

---
+++ src/components/LeftMenuLink.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-item clickable tag="a" :to="url">
+    <q-item-section v-if="icon" avatar>
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      {{ name }}
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  name: "LeftMenuLink",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+
+    url: {
+      type: String,
+      default: "#"
+    },
+
+    icon: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
--- src/layouts/LeftBar.vue
+++ src/layouts/LeftBar.vue
@@ -8,71 +8,21 @@
     >
       <q-list>
         <q-item-label header class="text-grey-8">
-          Essential Links
+          Menu Links
         </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <LeftMenuLink v-for="link in menus" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
   </div>
 </template>
 <script>
-const linksData = [
-  {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev"
-  },
-  {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework"
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev"
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev"
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev"
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev"
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev"
-  }
-];
-import EssentialLink from "components/EssentialLink.vue";
+import LeftMenuLink from "components/LeftMenuLink.vue";
 
 export default {
-  components: { EssentialLink },
+  components: { LeftMenuLink },
   created() {},
   data() {
-    return {
-      essentialLinks: linksData
-    };
+    return {};
   },
   computed: {
     isLeftDrawerOpen: {
@@ -82,6 +32,9 @@ export default {
       set() {
         this.$store.dispatch("layout/setLeftDrawerOpen");
       }
+    },
+    menus() {
+      return this.$store.getters["userdata/userMenus"];
     }
   }
 };
